b2a10fcae2d47b70234a6285c3914a1ad4587e2e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-12 21:00:51 +0900

    変更 - App.vueのロジックの中身をindex.vueへ移動させる

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,8 @@
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-
-export default {
-  components: {
-    HelloWorld,
-    TheWelcome
-  },
-  data() {
-    return {
-      hoge: 'fooooooooooo cat',
-      isShow: true,
-      arr: [1, 3, 5, 7],
-      car: {
-        name: 'kuruma',
-        speede: 'hayai',
-        price: 'takai'
-      },
-      count: 0,
-      isDisabled: true
-    }
-  },
-  methods: {
-    //関数はメソッドエリア？で作る
-    increment() {
-      this.count++
-    }
-  },
-  //computedはdataが変わったら反応して、戻値が動的に計算し直す
-  computed: {
-    double() {
-      return this.count * 2
-    },
-    superMessage() {
-      return `われわれは${this.hoge}だ。${this.double}個分のアンコリーノを差し出せ`
-    }
-  }
-}
-</script>
-
 <template>
+  <!--
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div v-if="isShow">
-      <!-- 属性を指定し、変数を指定することで、値の切り替えに反応できる -->
-      <input v-model="hoge" :disabled="isDisabled" />
-
-      {{ hoge }}
-    </div>
-
-    {{ superMessage }}
-
-    {{ arr[1] }}
-
-    {{ car.name }}
-
-    <!-- 動的にクラスを紐付ける 
-    dataの変数へ:classでアクセスして、hogeを呼び出す
-    -->
-    <div :class="hoge">うひょおおおおお</div>
-
-    <!-- イベントで変数へ値を代入する
-    この場合は、!で変数の反対にしてるので、trueとfalseを入れ替える -->
-    <button @click="isDisabled = !isDisabled">とぐる！！</button>
-
-    <div>{{ count }}</div>
-
-    countの2倍!!{{ double }}
-
-    <button @click="increment">+1</button>
-
-    <ul>
-      <li v-for="(item, key) in car" :key="key">{{ key }} - {{ item }}</li>
-    </ul>
-
-    <ul>
-      <li v-for="item in arr" :key="item"><TheWelcome /></li>
-    </ul>
-
-    <button @click="isShow = !isShow">toggle</button>
 
     <div class="wrapper">
       <HelloWorld msg="Riin Vue道場！" />
@@ -89,6 +12,7 @@ export default {
   <main>
     <TheWelcome />
   </main>
+-->
   <router-link to="/">home</router-link>
   <router-link to="/about">about</router-link>
   foooooooooooooooo cat
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,78 @@
+<script>
+export default {
+  data() {
+    return {
+      hoge: 'fooooooooooo cat',
+      isShow: true,
+      arr: [1, 3, 5, 7],
+      car: {
+        name: 'kuruma',
+        speede: 'hayai',
+        price: 'takai'
+      },
+      count: 0,
+      isDisabled: true
+    }
+  },
+  methods: {
+    //関数はメソッドエリア？で作る
+    increment() {
+      this.count++
+    }
+  },
+  //computedはdataが変わったら反応して、戻値が動的に計算し直す
+  computed: {
+    double() {
+      return this.count * 2
+    },
+    superMessage() {
+      return `われわれは${this.hoge}だ。${this.double}個分のアンコリーノを差し出せ`
+    }
+  }
+}
+</script>
+
 <template>
   <div>home</div>
+
+  <div v-if="isShow">
+    <!--
+    属性を指定し、変数を指定することで、値の切り替えに反応できる  -->
+    <input v-model="hoge" :disabled="isDisabled" />
+
+    {{ hoge }}
+  </div>
+
+  {{ superMessage }}
+
+  {{ arr[1] }}
+
+  {{ car.name }}
+
+  <!-- 動的にクラスを紐付ける 
+    dataの変数へ:classでアクセスして、hogeを呼び出す
+    -->
+  <div :class="hoge">うひょおおおおお</div>
+
+  <!-- イベントで変数へ値を代入する
+    この場合は、!で変数の反対にしてるので、trueとfalseを入れ替える -->
+  <button @click="isDisabled = !isDisabled">とぐる！！</button>
+
+  <div>{{ count }}</div>
+
+  countの2倍!!{{ double }}
+
+  <button @click="increment">+1</button>
+
+  <ul>
+    <li v-for="(item, key) in car" :key="key">{{ key }} - {{ item }}</li>
+  </ul>
+
+  <ul>
+    <li v-for="item in arr" :key="item">アンコリーノ</li>
+  </ul>
+
+  <button @click="isShow = !isShow">toggle</button>
+
   <router-link to="/about">Aboutへ</router-link>
 </template>
