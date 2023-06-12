8b33d2c543b74e3ca3ff0c3b143d6a3066de125e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-12 21:07:15 +0900

    変更 - Developコンポーネントをルーティングする

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import MainComponent from './pages/index.vue'
 import AboutComponent from './pages/about.vue'
+import DevelopComponent from './pages/develop.vue'
 
 const routes = [
   { path: '/', component: MainComponent },
-  { path: '/about', component: AboutComponent }
+  { path: '/about', component: AboutComponent },
+  { path: '/develop', component: DevelopComponent }
 ]
 
 const router = createRouter({
---
+++ src/pages/develop.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Develop</div>
+</template>
