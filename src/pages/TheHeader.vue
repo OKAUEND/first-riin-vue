0edc0d615e871365c3c2db0d9ddc80095d332432
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-12 21:21:08 +0900

    変更 - 見やすいようにスラッシュをいれる

--- src/pages/TheHeader.vue
+++ src/pages/TheHeader.vue
@@ -1,5 +1,4 @@
 <template>
-  <router-link to="/">Top</router-link>
-  <router-link to="/about">about</router-link>
+  <router-link to="/">Top</router-link>/ <router-link to="/about">about</router-link>/
   <router-link to="/develop">develop</router-link>
 </template>
