f468dee2e483526c8a683ea367147b039a85330c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-12 20:38:49 +0900

    変更 - 作ったコンポーネントをmain.jsで指定し、ルーティングする

--- src/main.js
+++ src/main.js
@@ -3,5 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import MainComponent from './pages/index.vue'
+import AboutComponent from './pages/about.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: MainComponent },
+  { path: '/about', component: AboutComponent }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
