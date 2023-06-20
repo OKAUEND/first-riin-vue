52c40ddf16080c1e29cdbcbf17b797df437e7acd
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-20 23:53:28 +0900

    変更 - vuetifyをつかい、UIを置き換える

--- src/pages/develop.vue
+++ src/pages/develop.vue
@@ -55,43 +55,68 @@ export default {
 </script>
 
 <template>
-  <div>Develop</div>
-  <div v-if="isShow">
-    <h2>アンコリーノ工場</h2>
-  </div>
-  {{ count }}個のアンコリーノ
-  <!-- @はこの後はイベントハンドラという宣言する構文
+  <section>
+    <v-card
+      title="アンコリーノとは"
+      text="アンコリーノは小麦粉で練った記事にアンコをハサミ鉄板で焼き上げたお菓子です。かのアウグストゥス・カエサルも好んで食べたと言われるお菓子の一つです"
+    >
+      <v-card-actions><v-btn>さぁ、アンコリーノを楽しもう</v-btn></v-card-actions>
+    </v-card>
+    <v-expansion-panels>
+      <v-expansion-panel
+        title="アンコリーノの歴史"
+        text="アンコリーノは紀元前1万年前に生まれた食べ物です。大判焼きや今川焼きの祖先でありローマ共和国やエジプト古代王朝でも親しまれたお菓子となります。"
+      >
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <v-divider :thickness="10" color="success"></v-divider>
+
+    <div v-if="isShow">
+      <h2>アンコリーノ工場</h2>
+    </div>
+    {{ count }}個のアンコリーノ
+
+    <v-list lines="one">
+      <v-list-item v-for="(item, index) in arr" :key="index"
+        ><v-btn @click="add(item)">{{ item }}個のアンコリーノを増やす</v-btn></v-list-item
+      >
+    </v-list>
+    <!-- @はこの後はイベントハンドラという宣言する構文
        その後のclickはクリックイベント-->
-  <button @click="division">/2</button>
-  <button @click="decrement">-1</button>
-  <button @click="add(1)">+1</button>
-  <button @click="add(2)">+2</button>
-  <button @click="add(3)">+3</button>
-  <!-- @click="count +2" は何も代入していないので結果虚無に消えている。ｼｮｯｷﾞｮﾑｯｼﾞｮ -->
-  <button @click="count = count + 2">+2</button>
-  <button @click="double">*2</button>
-  <button @mouseover="double">ふふふ</button>
-  <button @click="isShow = false">消えろ！アンコリーノ！</button>
-  <button @click="sample($event)">あんこリーノとは</button>
-  <ul>
-    <li v-for="(index, key) in number" :key="key">
-      <button @click="add(index)">今川焼きを{{ index }}個増やす</button>
-    </li>
-  </ul>
-  <ul>
-    <li v-for="(number, key) in arr" :key="key">
-      <button @click="add(number)">大判焼きを{{ number }}個増やす</button>
-    </li>
-  </ul>
+    <!-- <v-btn @click="division">/2</v-btn>
+    <v-btn @click="decrement">-1</v-btn>
+    <v-btn @click="add(1)">+1</v-btn>
+    <v-btn @click="add(2)">+2</v-btn>
+    <v-btn @click="add(3)">+3</v-btn> -->
+    <!-- @click="count +2" は何も代入していないので結果虚無に消えている。ｼｮｯｷﾞｮﾑｯｼﾞｮ -->
+    <!-- <v-btn @click="count = count + 2">+2</v-btn>
+    <v-btn @click="double">*2</v-btn>
+    <v-btn @mouseover="double">ふふふ</v-btn>
+    <v-btn @click="isShow = false">消えろ！アンコリーノ！</v-btn>
+    <v-btn @click="sample($event)">あんこリーノとは</v-btn>
+    <ul>
+      <li v-for="(index, key) in number" :key="key">
+        <v-btn @click="add(index)">今川焼きを{{ index }}個増やす</v-btn>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(number, key) in arr" :key="key">
+        <v-btn @click="add(number)">大判焼きを{{ number }}個増やす</v-btn>
+      </li>
+    </ul> -->
 
-  {{ message }}
-  <input v-model="tmpMessage" />
-  <button @click="addMessage()">はーーーーー！</button>
-  <button @click="delMessage()">きえろ！！</button>
-  {{ messages }}
+    <!-- {{ message }}
+    <input v-model="tmpMessage" />
+    <v-btn @click="addMessage()">はーーーーー！</v-btn>
+    <v-btn @click="delMessage()">きえろ！！</v-btn>
+    {{ messages }}
 
-  <ul>
-    <li v-for="(index, key) in messages" :key="key">{{ index }}</li>
-  </ul>
+    <ul>
+      <li v-for="(index, key) in messages" :key="key">{{ index }}</li>
+    </ul>
+
+    <v-btn>ボタン！</v-btn> -->
+  </section>
   <the-header></the-header>
 </template>
