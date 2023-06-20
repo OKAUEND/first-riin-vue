9db85fcf8536155967add12bb27c3820ea7f798c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-20 23:53:40 +0900

    変更 - セクションを追加して、並びを追加する

--- src/pages/TheHeader.vue
+++ src/pages/TheHeader.vue
@@ -1,4 +1,6 @@
 <template>
-  <router-link to="/">Top</router-link>/ <router-link to="/about">about</router-link>/
-  <router-link to="/develop">develop</router-link>
+  <section>
+    <router-link to="/">Top</router-link>/ <router-link to="/about">about</router-link>/
+    <router-link to="/develop">develop</router-link>
+  </section>
 </template>
